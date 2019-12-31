aebc2f33b91bb5ce74dbeeece1dd37315ce0a106
awr417h <abdullahwaqar8@gmail.com>
2019-12-31 19:08:42 +0500

    storage api

---
+++ storage.py
@@ -0,0 +1,22 @@
+import os
+from typing import List
+
+def getRootStorageAbsPath() -> str:
+    """
+    * @desc: Returns the root path for storage folder
+    * @return: Normalized path
+    """
+    return os.path.abspath('storage')
+
+def enumrateFilenames() -> List:
+    """
+    * @desc: Return a list of all the file names in the storage
+    * @return: List of file names
+    """
+    filenameList: List = []
+    for dirpath, dirnames, filenames in os.walk(getRootStorageAbsPath()):
+        for _, name in enumerate(filenames):
+            filenameList.append('{}/{}'.format(dirpath, name))
+    return filenameList
+
+print(enumrateFilenames())
