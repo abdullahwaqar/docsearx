cb3790c6139ac1f7fc4610423419c887cef06a26
awr417h <abdullahwaqar8@gmail.com>
2020-01-01 19:50:36 +0500

    added computations for tfidf

--- engine.py
+++ engine.py
@@ -1,4 +1,5 @@
 import re
+import collections
 from nltk import PorterStemmer
 from pdf_reader import readAllPdf
 
@@ -25,25 +26,74 @@ class Engine:
     def read_raw_data(self):
         """
         * @desc: Reads and process the raw data aka without stemming and get the data ready for processing
-        * @return: 
+        * @return: List of (filename, content) after stemming and preprocessing it
         """
         docs = []
+        title = []
         for filename, content in readAllPdf():
             current_filename = filename
-            lines = []
             content = content.lower() #* Making sure everything is lower
             content = [line.strip() for line in content.split()] #* Splitting on whitespaces
             content = [self.alphanum.sub('', word) for word in content] #* Removing non alphanumeric characters
             content = [word for word in content if word != ''] #* Remove any words that are now empty due to splitting them and and getting them through re
             content = [self.ps.stem(word) for word in content] #* Stem words
-            if current_filename == filename:
-                lines.extend(content)
-            if current_filename != filename:
-                docs.append((current_filename, lines))
-                current_filename = filename
-                continue
-        print(docs)
+            docs.append(content)
+            title.append(filename)
+
+        return title, docs
+
+    def read_data(self):
+        """
+        * @desc: Reads in the documents to be indexed.
+        * @return: None
+        """
+        titles, docs = self.read_raw_data()
+        #* Sort document alphabetically by title to ensure we have the proper
+        #* document indices when referring to them.
+        ordering = [idx for idx, title in sorted(enumerate(titles),
+        key = lambda xx : xx[1])]
+
+        self.titles = []
+        self.docs = []
+        numdocs = len(docs)
+        for d in range(numdocs):
+            self.titles.append(titles[ordering[d]])
+            self.docs.append(docs[ordering[d]])
+
+        #* Get the vocabulary.
+        self.vocab = [word for word in self.get_uniq_words()]
+
+    def compute_tfidf(self):
+        """
+        * @desc: Compute and store TF-IDF values for words and documents.
+        *
+        """
+        self.tfidf = {}
+        idf = {}
+        doc_cont = {}
+
+        for i, doc in enumerate(self.docs):
+            doc_cont[i] = collections.Counter(doc)
+        for word in self.vocab:
+            word_set = 0.0 + len(IRSystem.get_posting(self, word))
+            idf[word] = math.log10(len(self.docs) / word_set)
+            if word not in self.tfidf:
+                self.tfidf[word] = {}
+            for d in range(len(self.docs)):
+                tf = doc_cont[d][word]
+                if tf == 0.0:
+                    self.tfidf[word][d] = 0.0
+                else:
+                    self.tfidf[word][d] = (1.0 + math.log10(tf)) * idf[word]
+
+    def get_tfidf(self, word, document):
+        """
+        * @desc: Return the tf-idf weigthing for the given word (string) and document index.
+        """
+        tfidf = self.tfidf[word][document]
+        return tfidf
+
 
 if __name__ == "__main__":
-    Engine().read_raw_data()
+    Engine().read_data()
 
